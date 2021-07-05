27c30a2403b340c42ba95167794e37cbca4f91ba
Lerning <learningcomputer2019@gmail.com>
2021-07-05 13:50:03 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.frontend')
+@section('content')
+<section class="banner-area" id="home">
+  <div class="container">
+    <div class="row fullscreen d-flex align-items-center justify-content-center">
+      <div class="banner-content col-lg-6 col-md-6">
+        <h1>
+          Search <br>
+          Choose <br>
+          Buy
+        </h1>
+        <p class="text-white text-uppercase">
+          Everyone wants the innovation through books
+        </p>
+      </div>
+      <div class="banner-img col-lg-6 col-md-6">
+        <img class="img-fluid" src="https://tam.r.worldssl.net/wp-content/uploads/2017/06/best-books-for-men.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="products-area section-gap">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-12 pb-40 header-text text-center">
+        <h1 class="pb-10">Featured Books to Show</h1>
+        <p>
+          Who are in extremely love with books.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="single-product">
+              <div class="thumb">
+                <img src="user/img/p2.png" alt="">
+              </div>
+              <div class="details">
+                <h4>The Crab Wheel</h4>
+                <p>
+                  Who are in extremely love with eco friendly system.
+                </p>
+                <a href="#" class="primary-btn text-uppercase">View Details</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="single-product">
+              <div class="thumb">
+                <img src="user/img/p3.png" alt="">
+              </div>
+              <div class="details">
+                <h4>The Plug Ninja</h4>
+                <p>
+                  Who are in extremely love with eco friendly system.
+                </p>
+                <a href="#" class="primary-btn text-uppercase">View Details</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="single-product">
+              <div class="thumb">
+                <img src="user/img/p4.png" alt="">
+              </div>
+              <div class="details">
+                <h4>The Controller</h4>
+                <p>
+                  Who are in extremely love with eco friendly system.
+                </p>
+                <a href="#" class="primary-btn text-uppercase">View Details</a>
+              </div>
+            </div>
+          </div>
+          
+    </div>
+  </div>
+</section>
+
+<section class="feature-area section-gap">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="col-md-12 pb-40 header-text text-center">
+          <h1 class="pb-10">Featured Books to Show</h1>
+          <p>
+            Who are in extremely love with books.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-lg-3 col-md-6">
+              <div class="single-feature">
+                  <a href="#" class="title d-flex flex-row">
+                      <span class="lnr lnr-user"></span>
+
+                  <h4>Expert Technicians</h4>
+                  <p>
+                    Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                  </p>
+                  
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">
+                <div class="single-feature">
+                    <a href="#" class="title d-flex flex-row">
+                        <span class="lnr lnr-user"></span>
+  
+                    <h4>Professional Service</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                    
+                  </div>
+              </div>
+            
+              <div class="col-lg-3 col-md-6">
+                <div class="single-feature">
+                    <a href="#" class="title d-flex flex-row">
+                        <span class="lnr lnr-user"></span>
+  
+                    <h4>Great Support</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                    
+                  </div>
+              </div>
+
+              <div class="col-lg-3 col-md-6">
+                <div class="single-feature">
+                    <a href="#" class="title d-flex flex-row">
+                        <span class="lnr lnr-recket"></span>
+  
+                    <h4>Technical Skills</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                    
+                  </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="single-feature">
+                    <a href="#" class="title d-flex flex-row">
+                        <span class="lnr lnr-diamond"></span>
+  
+                    <h4>Highly Recomended</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                    
+                  </div>
+              </div>
+
+              <div class="col-lg-3 col-md-6">
+                <div class="single-feature">
+                    <a href="#" class="title d-flex flex-row">
+                        <span class="lnr lnr-bubble"></span>
+  
+                    <h4>Positive Review</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                    
+                  </div>
+              </div>
+
+      </div>
+    </div>
+  </section>
+@endsection
